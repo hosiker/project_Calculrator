17dd401fa9bcc04803a6ec5594f86c990adec305
이호식 <leehosick462589@gmail.com>
2023-07-20 03:09:27 +0900

    Lv2 finish

--- Lv1/app/src/main/java/com/example/calculator_lv1/Calculator.kt
+++
@@ -1,2 +0,0 @@
-package com.example.calculator_lv1
-
--- Lv2/app/src/main/java/com/example/calculator_lv2/Calculator_Lv2_main.kt
+++ Lv2/app/src/main/java/com/example/calculator_lv2/Calculator_Lv2_main.kt
@@ -1,2 +1,61 @@
 package com.example.calculator_lv2
 
+fun main(){
+
+    println("첫번째 숫자를 입력하시오")
+    val num1:Int = readLine()!!.toInt()
+
+    println("두번째 숫자를 입력하시오")
+    val num2:Int = readLine()!!.toInt()
+
+    println("연산기호를 입력하시오(+,-,*,/,%)")
+    val operator:String = readLine()!!.toString()
+
+    //println("${num1},${num2},${operator}")
+
+    val calculator = Calculator(num1, num2, operator)
+    calculator.result()
+}
+class Calculator(private val num1:Int, private val num2:Int, private val operator:String){
+
+    fun result(){
+        when(operator){
+            "+" -> {
+                val sum:Int = num1 + num2
+                println("결과는 ${sum}입니다.")
+            }
+            "-" -> {
+                val sum:Int = num1 - num2
+                println("결과는 ${sum}입니다.")
+            }
+            "*" -> {
+                val sum:Int = num1 * num2
+                println("결과는 ${sum}입니다.")
+            }
+            "/" -> {
+                if(num2!=0) {
+                    val sum: Int = num1 / num2
+                    println("결과는 ${sum}입니다.")
+                }
+                else{
+                    println("0으로 나눌 수 없습니다.")
+                }
+            }
+            "%" -> {
+                if(num2!=0) {
+                    val sum: Int = num1 % num2
+                    println("결과는 ${sum}입니다.")
+                }
+                else{
+                    println("0으로 나눌 수 없습니다.")
+                }
+            }
+            else->{
+                println("지원하지 않는 연산자입니다.")
+            }
+        }
+    }
+
+
+
+}
